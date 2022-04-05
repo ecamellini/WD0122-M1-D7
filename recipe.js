dcdbea76a75a7c8f6773f84cb7861f58e26b753e
Eric Camellini <eric.camellini@gmail.com>
2022-04-05 10:40:18 +0200

    Adding a note on loose typing

--- recipe.js
+++ recipe.js
@@ -13,6 +13,16 @@ let butter = 80
 let sugar = 120
 let flour = 200
 
+// flour = "200"
+// ^^^^^ JavaScript does not care if this was a number.
+// It will throw it away and put the string "200" in flour.
+// Even if you put "Jhon" instead of "200", JS will try to figure out what to do.
+// YOU SHOULD NEVER DO THIS. NEVER ASSIGN DIFFERENT TYPES OF DATA TO THE SAME VARIABLE.
+// WHY? Because you can cause unexpected bugs later in the code...
+// This was a number, and now we make it a string. Who knows what happens later...
+// JS will never complain. SO, YOU SHOULD BE CAREFUL ABOUT THIS.
+
+
 // We can give more than one thing to console.log, using the comma.
 // Everything will be printed on the same line with spaces in between.
 console.log("Butter:", butter) // This will print: "Butter: 80"
