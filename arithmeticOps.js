28ad16dd4a9395ae67cf70e7c0ad73402331280c
Eric Camellini <eric.camellini@gmail.com>
2022-04-05 10:40:40 +0200

    Adding example about arithmetic ops

---
+++ arithmeticOps.js
@@ -0,0 +1,10 @@
+console.log("----------------SOME ARITHMETIC OPERATORS---------------")
+console.log(5 + 8) // 13
+console.log(4 / 8) // 0.5
+console.log(3 * 8) // 24
+console.log(3 ** 3) // 27 - 3^3
+console.log(10 % 3) // 1
+
+console.log("\nHow to figure out if a number is even or odd?")
+console.log("Even number, remainder of the division by 2:", 10 % 2)
+console.log("Odd number, remainder of the division by 2:", 11 % 2)
